2d438791abf58536f3df12e8790df02086a39840
Josephine Een <josephine.een@appliedtechnology.se>
2020-10-29 20:48:12 +0100

    learning adv css animation on btn hover

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import NavigationLink from "./NavigationLink"
 
 const Navbar = styled.nav`
+margin-top:1rem;
   background-color: #2d2a2f;
   height: 64px;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
--- src/components/NavigationBar/NavigationLink.tsx
+++ src/components/NavigationBar/NavigationLink.tsx
@@ -1,50 +1,93 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import "typeface-playfair-display"
 
 const LinksContainer = styled.ul`
   list-style: none;
   display: flex;
-  margin:0;
-  padding:0 32px;
+  margin: 0;
+  padding: 0 32px;
   width: 100%;
+`
+const rotate = keyframes`
+  0% {
+    background-position: 100% 0, 0 0% , 0% 100%, 100% 100%;
+  background-size: 0 3px, 3px 0, 100% 3px, 3px 0;
+  }
+  40% {
+    background-position: 100% 0%, 0 0% , 100% 100%, 100% 100%;
+  background-size: 0% 3px, 3px 0, 0% 3px, 3px 100%;
+  }
+  60% {
+    background-position: 100% 0, 0 0% , 100% 100%, 100% 0%;
+  background-size: 100% 3px, 3px 0%, 0% 3px, 3px 100%;
+  }
+  70% {
+    background-position: 0 0, 0 0% , 100% 100%, 100% 100%;
+  background-size: 100% 3px, 3px 100%, 0 3px, 3px 0;
+  }
+  80% {
+    background-position: 0 0,  0 100% , 100% 100%, 100% 100%;
+  background-size: 0 3px, 3px 100%, 0 3px, 3px 0;
+  }
+
+  100% {
+    background-position: 100% 0%,  0 100% , 0% 100%, 100% 100%;
+  background-size: 0% 3px, 3px 0%, 100% 3px, 3px 0%;
+  }
 `
 const LinkItem = styled.li`
   color: white;
   font-size: 1.4rem;
   font-family: "Playfair Display", serif;
-  text-transform:capitalize;
-  width:16vw;
-  padding:8px;
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  position:relative;
+  text-transform: capitalize;
+  width: 16vw;
+  padding: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  background-image: linear-gradient(red, red), linear-gradient(yellow, yellow),
+    linear-gradient(blue, blue), linear-gradient(orange, orange);
+    background-position: 100% 0%, 0% 100% , 0% 100%, 100% 100%;
+  background-size: 0% 3px, 3px 0%, 0% 3px, 3px 0%;
+  background-color: transparent;
+  background-repeat: no-repeat;
+
+  transition: 3s linear;
   &::before {
-    content:"";
-    width:56%;
-    height:2px;
-    position:absolute;
-    bottom:16px;
-    background-color:#81FF70;
-    transform:scaleX(0);
-    transform-origin:left;
-    transition: 0.25s ease-in;
+    content: "";
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    border: 3px solid #60daaa;
   }
-  &:hover, &:hover::before {
-    transform:scale(1)
+  &:hover {
+    //[left, top]
+    background-position: 100% 0, 0 0% , 0% 100%, 100% 100%;
+  background-size: 0 3px, 3px 0, 0% 3px, 3px 0;
+    animation: ${rotate} 3s linear forwards 0.2s;
   }
 `
 const LogoItem = styled.li`
   font-size: 2rem;
   font-family: "Playfair Display", serif;
-  position:relative;
+  position: relative;
   top: 8px;
 `
 const renderLinks = (links: any) => {
   const middleLogoPos = Math.round(links.length / 2) - 1
-  const tempArr = links.map((item: string,index:number) => <LinkItem key={`${index}+${item}`}>{item}</LinkItem>)
-  tempArr[middleLogoPos] = <LogoItem key={`${middleLogoPos}+${links[middleLogoPos]}`}>{links[middleLogoPos]}</LogoItem>
+  const tempArr = links.map((item: string, index: number) => (
+    <LinkItem key={`${index}+${item}`}>{item}</LinkItem>
+  ))
+  tempArr[middleLogoPos] = (
+    <LogoItem key={`${middleLogoPos}+${links[middleLogoPos]}`}>
+      {links[middleLogoPos]}
+    </LogoItem>
+  )
   return tempArr
 }
 export default function NavigationLink({
